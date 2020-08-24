fe1f160543c0cc46251d51dd1a78aa54acabd63e
crtpi <64794262+crtpi@users.noreply.github.com>
2020-08-24 15:35:35 -0700

    Update win31.sh

--- dosbox_win31/win31.sh
+++ dosbox_win31/win31.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-/opt/retropie/emulators/dosbox/bin/dosbox -conf "/home/pi/RetroPie/roms/pc/win31.conf"
+/opt/retropie/emulators/dosbox/bin/dosbox -conf "/home/pi/RetroPie/roms/pc/WINDOWS/win31.conf"
